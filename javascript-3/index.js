874e1ebe337680ef91d5135fa315e82d1bc74606
Александр Летов <alexander.letov@lenta.com>
2021-07-26 17:38:18 +0300

    исправление 2

--- javascript-3/index.js
+++ javascript-3/index.js
@@ -1,38 +1,29 @@
 const getPath = (val) => {
     if (val == '' || val == null) return '';
-    // У текущего элемента есть id = возвращаем
     if (val.id) return `#${val.id}`; 
     
-    // Нету => строим дерево парентов
-    let elemetsArr = [];
+    const elemetsArr = [];
     elemetsArr.push(val);
     
     let parent = val.parentElement;
     while (parent) {
+        if (parent.tagName == "BODY" || parent.tagName == "HTML") break;
         elemetsArr.push(parent);
         parent = parent.parentElement;
     }
     
-    // var parent = val.parentNode;
-    // var index = parent.children.indexOf(child)
-    // var index = Array.prototype.indexOf.call(parent.children, val);
-    
-    let pathArr = []; // Путь до элемента
+    const pathArr = [];
     let idIndex = elemetsArr.findIndex(element=>element.id);
     if (idIndex > -1) {
-        // Нашёлся id в цепочке
         for (let index = 0; index < idIndex+1; index++) {
             const element = elemetsArr[index];
-            if (element.tagName.toLowerCase() == "body" || element.tagName.toLowerCase() == "html") break;
 
-            // соседи и tagName
             let idInNeighbors = Array.prototype.indexOf.call(element.parentElement.children, element); 
             if (idInNeighbors != 0)  
-                pathArr.push(`${element.tagName}:nth-child(${idInNeighbors+1})`); // соседи есть
+                pathArr.push(`${element.tagName}:nth-child(${idInNeighbors+1})`); 
             else  
-                pathArr.push(element.tagName); // соседей нету
+                pathArr.push(element.tagName);
 
-            // классы
             if (element.className != null || element.className != "") {
                 let className = element.className;
                 className = className.split(" ").filter(el => el != "").map(el => `.${el}`).join(' > ');
@@ -45,19 +36,15 @@ const getPath = (val) => {
             }
         }
     } else {
-        // Составляем дерево классов и тэгов
         for (let index = 0; index < elemetsArr.length; index++) {
             const element = elemetsArr[index];
-            if (element.tagName == "BODY" || element.tagName == "HTML") break;
 
-            // соседи и tagName
             let idInNeighbors = Array.prototype.indexOf.call(element.parentElement.children, element);
             if (idInNeighbors != 0)  
-                pathArr.push(`${element.tagName}:nth-child(${idInNeighbors+1})`); // соседи есть
+                pathArr.push(`${element.tagName}:nth-child(${idInNeighbors+1})`);
             else  
-                pathArr.push(element.tagName); // соседей нету
+                pathArr.push(element.tagName); 
 
-            // классы
             if (element.className != null || element.className != "") {
                 let className = element.className;
                 className = className.split(" ").filter(el => el != "").map(el => `.${el}`).join(' > ');
@@ -71,7 +58,3 @@ const getPath = (val) => {
 }
 
 module.exports = getPath;
-
-
-// Здравствуйте! А что если рядом с элементом будут находиться другие элементы на том же уровне? и мы захотим не первый параграф найти, а пятый?
-// Ваши тесты тоже корректно отработают и в этом случае, поэтому я хотел бы, чтобы Вы добавили поддержку nth-child и написали более сложные тесты для проверки различных кейсов
